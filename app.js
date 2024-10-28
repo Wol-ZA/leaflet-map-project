6d6439b899ff37dc50c67c397acc26d27010b818
Johan <102482684+Wol-ZA@users.noreply.github.com>
2024-10-28 20:22:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 require([
   "esri/Map",
-  "esri/views/SceneView",
-  "esri/layers/ElevationLayer",
+  "esri/views/MapView",
   "esri/layers/SceneLayer"
-], function(Map, SceneView, ElevationLayer, SceneLayer) {
+], function(Map, MapView, SceneLayer) {
   
-  // Create a 3D map with the "topo" basemap
+  // Create a 2D map with the "topo" basemap
   const map = new Map({
-    basemap: "topo",
-    ground: "world-elevation" // Adds elevation to the ground
+    basemap: "topo" // Use a 2D basemap
   });
 
-  // Create the 3D SceneView
-  const view = new SceneView({
-    container: "viewDiv", // Reference to the container element
-    map: map,             // Reference to the Map object created before the view
-    center: [-122.4194, 37.7749], // Longitude, latitude (San Francisco)
-    zoom: 12,             // Zoom level
-    tilt: 60              // Tilt the view to see 3D buildings
+  // Create the 2D MapView
+  const view = new MapView({
+    container: "viewDiv",             // Reference to the container element
+    map: map,                         // Reference to the Map object created before the view
+    center: [22.4617, -33.9646],      // Longitude, latitude (George, South Africa)
+    zoom: 12                          // Zoom level
   });
 
-  // Add a 3D building layer (optional)
+  // Add a 3D building layer as 2D feature (optional)
   const buildingsLayer = new SceneLayer({
     portalItem: {
       id: "f2e9b762544945f390ca4ac3671cfa72" // Sample building layer from ArcGIS
