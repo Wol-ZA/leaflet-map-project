e81e2c533700f9edd2afefb5a4e399640da6f195
Johan <102482684+Wol-ZA@users.noreply.github.com>
2024-11-11 13:11:35 +0200

    Update Flight.js

--- Flight.js
+++ Flight.js
@@ -377,7 +377,14 @@ window.addMarkersAndDrawLine = function(data) {
             const mapPoint = view.toMap({ x: event.x, y: event.y });
             if (mapPoint) {
                 console.log("Moving marker to:", mapPoint); // Debugging line
+                // Remove the old marker and re-add the updated one
+                graphicsLayer.remove(markerGraphics[activeMarkerIndex]);
+
+                // Update marker geometry
                 markerGraphics[activeMarkerIndex].geometry = mapPoint;
+
+                // Re-add the marker
+                graphicsLayer.add(markerGraphics[activeMarkerIndex]);
 
                 // Update the polyline coordinates
                 polylineCoordinates[activeMarkerIndex] = [mapPoint.longitude, mapPoint.latitude];
@@ -396,6 +403,7 @@ window.addMarkersAndDrawLine = function(data) {
         }
     });
 };
+
 
 
 
