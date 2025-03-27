13b1c1223332f47ec5d89dc83833b98a54e86248
Johan <102482684+Wol-ZA@users.noreply.github.com>
2025-03-27 13:53:17 +0200

    Update Trip.js

--- Trip.js
+++ Trip.js
@@ -57,17 +57,6 @@ window.loadFlightPath = function(flightData) {
         return [longitude, latitude, altitude];
     });
       // **Plot Waypoints**
-       const markerSymbol = new SimpleMarkerSymbol({
-                color: [0, 0, 255, 0.6],
-                size: 8,
-                outline: { color: "white", width: 2 }
-            });
-
-            const pointGraphic = new Graphic({
-                geometry: point,
-                symbol: markerSymbol
-            });
-            graphicsLayer.add(pointGraphic);
     
     flightData.forEach(({ latitude, longitude, altitude }) => {
     const verticalLine = new Polyline({
